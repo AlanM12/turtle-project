d226eee4d9927d027923d84568c2889c1fad9918
AlanM12 <alanmesseh@gmail.com>
2024-01-19 12:32:02 -0500

    Update main.py
    Uploading code implementation to the main program

--- main.py
+++ main.py
@@ -1,2 +1,61 @@
 # Turtle Project
 # Author: Alan Messeh
+
+
+import turtle
+turtle.shape("turtle") # optional
+
+def back(len):
+    turtle.penup()
+    turtle.backward(len)
+    turtle.pendown()
+def square(len):
+    for i in range(4):
+        turtle.forward(len)
+        turtle.right(90)
+
+def triangle(len):
+    for i in range(3):
+        turtle.forward(len)
+        turtle.left(120)
+
+def house(len):
+    square(len)
+    triangle(len)
+def main():
+    turtle.speed(0)
+    turtle.color("red")
+    house(150)
+    turtle.color("blue")
+    turtle.penup()
+    turtle.forward(200)
+    turtle.pendown()
+    house(100)
+
+turtle.speed(0) # optional
+turtle.color("red")
+
+main()
+turtle.Screen().exitonclick()
+# #def shape
+# turtle.pensize(5)
+# turtle.forward(90)
+# turtle.left(90)
+# turtle.forward(90)
+# turtle.left(90)
+# turtle.forward(90)
+# turtle.left(90)
+# turtle.forward(90)
+#
+# turtle.penup()
+# turtle.pendown()
+#
+# turtle.color("blue")
+# turtle.left(90)
+# turtle.forward(250)
+# turtle.left(90)
+# turtle.forward(90)
+# turtle.left(90)
+# turtle.forward(90)
+# turtle.left(90)
+# turtle.forward(90)
